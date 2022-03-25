0143ba2ea927a6708325392c337782ab6e668094
Elijah Wang <elijah725@gmail.com>
2022-03-25 23:09:33 +0800

    search.cs DI

--- Aircnc.FrontStage/Services/Guest/SearchRoom.cs
+++ Aircnc.FrontStage/Services/Guest/SearchRoom.cs
@@ -1,6 +1,13 @@
-﻿namespace Aircnc.FrontStage.Services.Guest
+﻿using Aircnc_0321.Repositories;
+
+namespace Aircnc.FrontStage.Services.Guest
 {
     public class SearchRoom
     {
+        private readonly DBRepository _DBRepository;
+        public SearchRoom(DBRepository dBRepository)
+        {
+            _DBRepository = dBRepository;
+        }
     }
 }
