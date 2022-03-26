de4b40abce110586ad1282771da578a7f9c7f42d
Elijah Wang <elijah725@gmail.com>
2022-03-26 16:21:58 +0800

    edit method GetRoom in SearchRoom Service

--- Aircnc.FrontStage/Services/Guest/SearchRoom.cs
+++ Aircnc.FrontStage/Services/Guest/SearchRoom.cs
@@ -1,4 +1,9 @@
-﻿using Aircnc_0321.Repositories;
+﻿using Aircnc.FrontStage.Models.Dtos.Guest;
+using Aircnc.FrontStage.Models.Entities;
+using Aircnc_0321.Repositories;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Aircnc.FrontStage.Services.Guest
 {
@@ -8,6 +13,28 @@ namespace Aircnc.FrontStage.Services.Guest
         public SearchRoom(DBRepository dBRepository)
         {
             _DBRepository = dBRepository;
+        }
+
+        public IEnumerable<SearchRoomResult> GetRoom(string location)
+        {
+            return _DBRepository.GetAll<Room>().Where(room => room.City == location).Select(room => new SearchRoomResult
+            {
+                RoomId = room.RoomId,
+                UserId = room.UserId,
+                HouseType = room.HouseType,
+                RoomType = room.RoomType,
+                Status = room.Status,
+                RoomName = room.RoomName,
+                Pax = room.Pax,
+                RoomCount = room.RoomCount,
+                BedCount = room.BedCount,
+                BathroomCount = room.BathroomCount,
+                Country = room.Country,
+                City = room.City,
+                District = room.District,
+                UnitPrice = room.UnitPrice,
+
+            });
         }
     }
 }
