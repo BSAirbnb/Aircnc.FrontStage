9491699a04168d0e520783a75bbd6f991d1e7ffd
Sean <kirirayray@gmail.com>
2022-03-29 07:53:19 +0800

    ADD Personal

---
+++ Aircnc.FrontStage/Controllers/PersonalController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aircnc.FrontStage.Controllers
+{
+    public class PersonalController : Controller
+    {
+        public IActionResult Index1()
+        {
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+        public IActionResult Index7()
+        {
+            return View();
+        }
+
+        public IActionResult Index8()
+        {
+            return View();
+        }
+
+        public IActionResult Index9()
+        {
+            return View();
+        }
+
+        public IActionResult Index99()
+        {
+            return View();
+        }
+    }
+}
