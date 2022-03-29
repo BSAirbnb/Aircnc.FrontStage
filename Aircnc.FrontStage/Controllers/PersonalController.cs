a0cb14dd60f9cbc08dcefbf31ff8a67f18ee0864
Sean <kirirayray@gmail.com>
2022-03-29 17:11:06 +0800

    ADD 個人資料

--- Aircnc.FrontStage/Controllers/PersonalController.cs
+++ Aircnc.FrontStage/Controllers/PersonalController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Aircnc.FrontStage.Models.Entities;
+using Aircnc.FrontStage.Models.ViewModels.Personal;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,30 @@ namespace Aircnc.FrontStage.Controllers
 {
     public class PersonalController : Controller
     {
-        public IActionResult Index1()
+        private readonly AircncContext _db;
+
+        public PersonalController(AircncContext db) 
         {
-            return View();
+            _db = db;
+        
+        }
+        public IActionResult Index1(int userid)
+        {
+            userid = 1;
+            var target = _db.Users.FirstOrDefault(user => user.UserId == userid);
+            var result = new PersonalViewModel
+            {
+                Name=target.Name,
+                Address=target.Address,
+                Email=target.Email,
+                Gender=target.Gender,
+                Phone=target.Phone,
+                Birthday = target.Birthday.ToString("yyyy/MM/dd"),
+                EmergencyContactName=target.EmergencyContactName,
+
+
+            };
+            return View(result);
         }
 
         public IActionResult Index2()
