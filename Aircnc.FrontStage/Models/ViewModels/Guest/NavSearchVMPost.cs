79b4a40c463d2e12cc646cecc5e735473c052d85
Alvin Wu <gravivector@gmail.com>
2022-04-03 23:05:44 +0800

    人數按鈕可用。navSearch action 暫時寫好

--- Aircnc.FrontStage/Models/ViewModels/Guest/NavSearchVMPost.cs
+++ Aircnc.FrontStage/Models/ViewModels/Guest/NavSearchVMPost.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace Aircnc.FrontStage.Models.ViewModels
 {
-    public class NavSearchViewModel
+    public class NavSearchVMPost
     {
         [StringLength(50)]
         public string Location { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
-        public int NumberOfGuests { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int? NumberOfGuests { get; set; }
     }
 }
