8989dcfa7b1de5a3bee2e019dd2dd4c94487b973
Vicky <hsiaovicky567@gmail.com>
2022-04-09 19:29:33 +0800

    order版面修改還沒改完controller先暫時讓我代userId=1

--- Aircnc.FrontStage/Controllers/OrderController.cs
+++ Aircnc.FrontStage/Controllers/OrderController.cs
@@ -19,10 +19,10 @@ namespace Aircnc.FrontStage.Controllers
         }
 
         [Authorize]
-        public IActionResult OrderList() //透過使用者ID去找他的訂單們
+        public IActionResult OrderList(int userId) //透過使用者ID去找他的訂單們
         {
-            //userId = 1;
-            int userId = int.Parse(User.Identity.Name);
+            userId = 1;
+            //int userId = int.Parse(User.Identity.Name);
             var orderList = _orderService.GetAllOrderByUserId(userId).Select(x => new OrderViewModel
             {
                 CkeckIn = x.CkeckIn.ToString("yyyy/MM/dd"),
