0ed1d2b894781817eca5c494c4981d9e9b36209a
Vicky <hsiaovicky567@gmail.com>
2022-04-11 00:16:07 +0800

    普天同慶訂單圖片終於修好了讚讚讚讚讚然後還加了分頁效果可是不知道為什麼第一頁沒東西我也是很問號

--- Aircnc.FrontStage/Controllers/OrderController.cs
+++ Aircnc.FrontStage/Controllers/OrderController.cs
@@ -13,16 +13,19 @@ namespace Aircnc.FrontStage.Controllers
     public class OrderController : Controller
     {
         private readonly OrderService _orderService;
+        private static int totalRows;
+        int Pages = 0;
+
         public OrderController(OrderService orderService)
         {
             _orderService = orderService;
         }
 
         [Authorize]
-        public IActionResult OrderList(int userId) //透過使用者ID去找他的訂單們
+        public IActionResult OrderList(int id = 1) //透過使用者ID去找他的訂單們
         {
-            userId = 1;
-            //int userId = int.Parse(User.Identity.Name);
+            //這邊先撈所有訂單資料
+            int userId = int.Parse(User.Identity.Name);
             var orderList = _orderService.GetAllOrderByUserId(userId).Select(x => new OrderViewModel
             {
                 CkeckIn = x.CkeckIn.ToString("yyyy/MM/dd"),
@@ -36,7 +39,31 @@ namespace Aircnc.FrontStage.Controllers
                 Status = x.Status
 
             });
-            return View(orderList);
+
+            if (totalRows == 0)
+            {
+                totalRows = _orderService.GetAllOrderByUserId(userId).Count();
+            }
+
+            int activePage = id; //目前所在分頁
+            int pageRows = 2; //每頁幾筆資料
+
+            //計算頁數
+            if (totalRows % pageRows == 0)
+            {
+                Pages = totalRows / pageRows;
+            }
+            else
+            {
+                Pages = (totalRows / pageRows) + 1;
+            }
+            int startRow = (activePage - 1) * pageRows; //紀錄起始index
+            var result = orderList.OrderBy(x => x.CkeckIn).Skip(startRow).Take(pageRows);
+            
+            ViewData["ActivePage"] = id; //active分頁碼
+            ViewData["Pages"] = Pages; //總頁數
+
+            return View(result);
         }
 
     }
