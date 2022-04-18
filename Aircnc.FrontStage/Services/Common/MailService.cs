779c3bc6cbe1971a602f7128b377fdf77b13267e
Ron <mope3128@gmail.com>
2022-04-18 19:28:53 +0800

    信箱

--- Aircnc.FrontStage/Services/Common/MailService.cs
+++ Aircnc.FrontStage/Services/Common/MailService.cs
@@ -19,19 +19,19 @@ namespace Aircnc.FrontStage.Services.Common
         {
             //SMTP @gmail.com
             SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
-            client.Credentials = new NetworkCredential("bsdevteam01@gmail.com", "bs53741405P@ssw0rd");
+            client.Credentials = new NetworkCredential("slipperyyyyy1231@gmail.com", "bmfnjyyhfzxgabba");
             client.EnableSsl = true;
 
             //Mail
             MailMessage mail = new MailMessage();
-            mail.From = new MailAddress("bsdevteam01@gmail.com", "Aircnc_Official");
+            mail.From = new MailAddress("slipperyyyyy1231@gmail.com", "Aircnc_Official");
             mail.To.Add(mailTo);
             mail.Priority = MailPriority.Normal;
             mail.Subject = "Aircnc_驗證信";
             mail.IsBodyHtml = true;
             mail.Body = @$"
                 <h1>點擊以下連結，啟用帳戶</h1>
-                <a href='https://{_httpContextAccessor.HttpContext.Request.Host.Value}/Account/Verify?user={userId}' target='_blank'>驗證</a>
+                <a href='https://{_httpContextAccessor.HttpContext.Request.Host.Value}/Account/Verify?user={userId}' target='_blank'>點此驗證帳號</a>
             ";
 
             client.Send(mail);
