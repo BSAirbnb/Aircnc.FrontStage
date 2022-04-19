72716b32efddb6866787a455d5c95040924f4a06
Alvin Wu <gravivector@gmail.com>
2022-04-19 21:43:56 +0800

    修改room detail 預訂按鈕，post 到 booking action

--- Aircnc.FrontStage/Controllers/Guest/BookingController.cs
+++ Aircnc.FrontStage/Controllers/Guest/BookingController.cs
@@ -1,16 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Aircnc.FrontStage.Models.ViewModels.Guest;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Aircnc.FrontStage.Controllers.Guest
 {
     public class BookingController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Booking()
         {
             return View();
         }
 
-        public IActionResult Booking()
+        [HttpPost]
+        public IActionResult Booking(SearchVM input)
         {
+            if (TempData["roomId"] != null) { var roomId = (int)TempData["roomId"]; }
+            if (ModelState.IsValid)
+            {
+
+            }
             return View();
         }
     }
