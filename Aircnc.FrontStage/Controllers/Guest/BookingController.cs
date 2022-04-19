f313c5dd5d4c3298831d10bff79b64a0ed009f5b
Elijah Wang <elijah725@gmail.com>
2022-04-19 23:07:35 +0800

    book

--- Aircnc.FrontStage/Controllers/Guest/BookingController.cs
+++ Aircnc.FrontStage/Controllers/Guest/BookingController.cs
@@ -1,10 +1,17 @@
 ﻿using Aircnc.FrontStage.Models.ViewModels.Guest;
 using Microsoft.AspNetCore.Mvc;
+using Aircnc.FrontStage.Services.Guest;
+using System.Linq;
 
 namespace Aircnc.FrontStage.Controllers.Guest
 {
     public class BookingController : Controller
     {
+        private readonly RoomDetailService _roomDetailService;
+        public BookingController(RoomDetailService roomDetailService)
+        {
+            _roomDetailService = roomDetailService;
+        }
         public IActionResult Booking()
         {
             return View();
@@ -16,9 +23,22 @@ namespace Aircnc.FrontStage.Controllers.Guest
             if (TempData["roomId"] != null) { var roomId = (int)TempData["roomId"]; }
             if (ModelState.IsValid)
             {
-
+                int roomId = 22;
+                var room = _roomDetailService.GetRoomDetailById(roomId);
+                var detail = new RoomDetailViewModel()
+                {
+                    OwnerName = room.OwnerName,
+                    RoomType = room.RoomType,
+                    HouseType = room.HouseType,
+                    District = room.District,
+                    City = room.City,
+                    RoomName = room.RoomName,
+                    AvgStars = room.AvgStars,
+                    ReviewsCount = room.Reviews.Count
+                };
+                input.RoomDetailVM = detail;
             }
-            return View();
+            return View(input);
         }
     }
 }
